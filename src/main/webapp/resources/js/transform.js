0c8226e78d7e388bbbd6f9337727a5aabfac5481
Jorge Morando <jmorando@cuyum.com>
2013-05-16 17:57:14 -0300

    Bugfix remote

--- src/main/webapp/resources/js/transform.js
+++ src/main/webapp/resources/js/transform.js
@@ -95,7 +95,7 @@ var setupValidations = function(field){
 			}else if(constraint.indexOf("depends=")!=-1){/*dependency*/
 				var dependency = constraint.substring(constraint.indexOf("depends=")+8);
 				f.data("jr:constraint:depends",dependency);
-			}else if(constraint.indexOf(" url=")!=-1){/*remote combo data*/
+			}else if(constraint.indexOf("url=")!=-1){/*remote combo data*/
 				var url = constraint.substring(constraint.indexOf("url=")+4);
 				f.data("jr:constraint:remote",url);
 			}else if(constraint.indexOf("type=cuit")!=-1){/*remote combo data*/
