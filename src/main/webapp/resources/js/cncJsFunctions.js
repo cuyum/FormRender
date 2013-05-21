4937539e7100370eaaf3b014821bb8971991b217
Jorge Morando <jmorando@cuyum.com>
2013-05-21 16:13:16 -0300

    Fixed #23 Se ha agregado un converter que parsea en locale es-AR los números. Las librerías agregadas de jquery permitirían implementar un converter de fechas y porcentajes i18n a nivel aplicación

---
+++ src/main/webapp/resources/js/cncJsFunctions.js
@@ -0,0 +1,32 @@
+var cncToNumber = function(value){
+	if(value && isNaN(value) && value.trim().length>0){
+		var number = $.i18n.parseNumber( value ,{region:'es-AR'});
+		return number;
+	}else if(!isNaN(value)){
+		console.warn("Number passed to cnctoNumber, only String value allowed");
+		return value;
+	}else if(value==undefined){
+		console.error("Undefined value passed to cncToNumber");
+		return value;
+	}
+};
+
+var cncFromNumber = function(value,decimals){
+	var d = 0;
+	var vStr = ""+value;
+	if(vStr.indexOf(".")>0){
+		d = vStr.substring(vStr.indexOf(".")+1).length;
+	}
+	decimals = "n"+d;
+	
+	if(value && !isNaN(value)){
+		var formated = $.i18n.formatNumber( value, decimals, {region:'es-AR'} );
+		return formated;
+	}else if(isNaN(value)){
+		console.warn("String passed to cncFromNumber, only numeric value allowed");
+		return value;
+	}else if(value==undefined){
+		console.error("Undefined value passed to cncFromNumber");
+		return value;
+	}
+};
