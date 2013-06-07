923c778ce412d9c6631b8d57ed3c9df9a57b4c63
Marta Oviedo <moviedo@cuyum.com>
2013-06-07 10:11:19 -0300

    Formularios Anexo D

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+-- ANEXO C --
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.1','Áreas de prestación de servicios','C1.001-V001-MAY13','C1.1.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.2','Interconexión','C1.002-V001-MAY13','C1.2.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.3','Redes e Infraestructura','C1.003-V001-MAY13','C1.3.xml', 0);
@@ -51,4 +52,53 @@ INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.51','Comercio Exterior','C1.051-V001-MAY13','C1.51.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.52','Rentas','C1.052-V001-MAY13','C1.52.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.53','Financiamiento SU / Subsidios','C1.053-V001-MAY13','C1.53.xml', 0);
+
+-- ANEXO D --
+
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.1','Áreas de prestación de servicios','D1.001-V001-MAY13','D1.1.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.2','Interconexión','D.002-V001-MAY13','D1.2.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.3','Redes e Infraestructura','D1.003-V001-MAY13','D1.3.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.4','Accesos','D1.004-V001-MAY13','D1.4.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.5','Clientes','D1.005-V001-MAY13','D1.5.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.6','Tráfico I (minutos)','D1.006-V001-MAY13','D1.6.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.7','Tráfico II (minutos)','D1.007-V001-MAY13','D1.7.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.8','Tráfico III (minutos)','D1.008-V001-MAY13','D1.8.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.9','Tráfico IV (minutos)','D1.009-V001-MAY13','D1.9.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.10','Tráfico V (llamadas)','D1.010-V001-MAY13','D1.10.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.11','Tráfico VI (llamadas)','D1.011-V001-MAY13','D1.11.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.12','Tráfico hacia Redes fijas I (minutos)','D1.012-V001-MAY13','D1.12.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.13','Tráfico hacia Redes fijas II (llamadas)','D1.013-V001-MAY13','D1.13.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.14','Trafico hacia Redes móviles I (minutos)','D1.014-V001-MAY13','D1.14.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.15','Tráfico hacia Redes móviles II (llamadas) ','D1.015-V001-MAY13','D1.15.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.16','Tráfico de datos','D1.016-V001-MAY13','D1.16.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.17','Roaming (minutos)','D1.017-V001-MAY13','D1.17.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.18','Interconexión (minutos)','D1.018-V001-MAY13','D1.18.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.19','Mensajes','D1.019-V001-MAY13','D1.19.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.20','Calidad del servicio / Ratios','D1.020-V001-MAY13','D1.20.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.21','Calidad del servicio / Facturación','D1.021-V001-MAY13','D1.21.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.22','Calidad del servicio / Reclamos','D1.022-V001-MAY13','D1.22.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.23','Calidad del servicio / Atención personalizada','D1.023-V001-MAY13','D1.23.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.24','Calidad del servicio / Atención telefónica','D1.024-V001-MAY13','D1.24.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.25','Calidad del Servicio / Atención web','D1.025-V001-MAY13','D1.25.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.26','Tasas generales','D1.026-V001-MAY13','D1.26.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.27','Tarifas / Planes pospago - mixto','D1.027-V001-MAY13','D1.27.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.28','Tarifas / Planes prepago','D1.028-V001-MAY13','D1.28.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.29','Tarifas / Roaming - LDN','D1.029-V001-MAY13','D1.29.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.30','Tarifas LDI','D1.030-V001-MAY13','D1.30.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.31','Tarifas / Planes internet móvil','D1.031-V001-MAY13','D1.31.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.32','Tarifas / Servicios adicionales','D1.032-V001-MAY13','D1.32.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.33','Ingresos prestación de servicios / Abonos telefonía móvil','D1.033-V001-MAY13','D1.33.xml', 0);
+-- INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.34','','D1.034-V001-MAY13','D1.34.xml', 0);
+-- INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.35','','D1.035-V001-MAY13','D1.35.xml', 0);
+-- INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.36','','D1.036-V001-MAY13','D1.36.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.37','Ingresos por Provincia','D1.037-V001-MAY13','D1.37.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.38','Gastos en Personal','D1.038-V001-MAY13','D1.38.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.39','Otros Gastos','D1.039-V001-MAY13','D1.39.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.40','Costos de interconexión','D1.040-V001-MAY13','D1.40.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.41','Personal ocupado','D1.041-V001-MAY13','D1.41.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.42','Inversiones','D1.042-V001-MAY13','D1.42.xml', 0);
+--INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.43','','D1.043-V001-MAY13','D1.43.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.44','Rentas','D1.044-V001-MAY13','D1.44.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.45','Financiamiento SU / Subsidios','D1.045-V001-MAY13','D1.45.xml', 0);
+
 
