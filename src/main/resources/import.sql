f1ddbced0020ad6f4aaf29fa25b93fedc2efed2f
Marta Oviedo <moviedo@cuyum.com>
2013-07-05 12:54:13 -0300

    Fix #186

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -45,7 +45,7 @@ INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.44','Ingresos interconexión a la red fija - LDN a vinculadas','C1.044-V001-MAY13','C1.44.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.45','Ingresos interconexión a la red fija - LDI a vinculadas','C1.045-V001-MAY13','C1.45.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.46','Gastos en Personal','C1.046-V001-MAY13','C1.46.xml', 0);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.47','Otros Gastos','C1.047-V001-MAY13','C1.47.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.47','Gastos y Costos','C1.047-V001-MAY13','C1.47.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.48','Costos interconexión','C1.048-V001-MAY13','C1.48.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.49','Personal ocupado','C1.049-V001-MAY13','C1.49.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('C1.50','Inversiones','C1.050-V001-MAY13','C1.50.xml', 0);
@@ -93,7 +93,7 @@ INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.36','Otros ingresos','D1.036-V001-MAY13','D1.36.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.37','Ingresos por Provincia','D1.037-V001-MAY13','D1.37.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.38','Gastos en Personal','D1.038-V001-MAY13','D1.38.xml', 0);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.39','Otros Gastos','D1.039-V001-MAY13','D1.39.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.39','Gastos y Costos','D1.039-V001-MAY13','D1.39.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.40','Costos de interconexión','D1.040-V001-MAY13','D1.40.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.41','Personal ocupado','D1.041-V001-MAY13','D1.41.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('D1.42','Inversiones','D1.042-V001-MAY13','D1.42.xml', 0);
@@ -144,7 +144,7 @@ INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.21','Ingresos por velocidad','F1.021-V001-MAY13','F1.21.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.22','Ingresos por provincia','F1.022-V001-MAY13','F1.22.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.23','Gastos','F1.023-V001-MAY13','F1.23.xml', 0);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.24','Otros Gastos','F1.024-V001-MAY13','F1.24.xml', 0);
+INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.24','Gastos y Costos','F1.024-V001-MAY13','F1.24.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.25','Otros Costos','F1.025-V001-MAY13','F1.25.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.26','Personal','F1.026-V001-MAY13','F1.26.xml', 0);
 INSERT INTO formulario (codigo, nombre, formVersion, archivo, version) VALUES ('F1.27','Inversiones','F1.027-V001-MAY13','F1.27.xml', 0);
