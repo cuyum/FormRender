12c894ffafd99eac5dbf91b2deb7f60915e033ee
Jorge Morando <jmorando@cuyum.com>
2013-07-12 15:54:16 -0300

    Soluciona required de combos

--- src/main/webapp/resources/js/cncFieldValidations.js
+++ src/main/webapp/resources/js/cncFieldValidations.js
@@ -652,18 +652,18 @@ var setupValidationDefaults = function(){
 	
 	$.validator.addMethod("cuit",
 		function(value,element){
-		    inputString = value.toString();
+			inputString = value.toString();
 		    if (inputString.length == 11) {
 		        var Caracters_1_2 = inputString.charAt(0) + inputString.charAt(1);
 		        if (Caracters_1_2 == "20" || Caracters_1_2 == "23" || Caracters_1_2 == "24" || Caracters_1_2 == "27" || Caracters_1_2 == "30" || Caracters_1_2 == "33" || Caracters_1_2 == "34") {
 		            var Count = inputString.charAt(0) * 5 + inputString.charAt(1) * 4 + inputString.charAt(2) * 3 + inputString.charAt(3) * 2 + inputString.charAt(4) * 7 + inputString.charAt(5) * 6 + inputString.charAt(6) * 5 + inputString.charAt(7) * 4 + inputString.charAt(8) * 3 + inputString.charAt(9) * 2 + inputString.charAt(10) * 1;
 		            Division = Count / 11;
 		            if (Division == Math.floor(Division)) {
-		                return true;
+		                return this.optional(element) || true;
 		            }
 		        }
 		    }
-		    return false;
+		    return this.optional(element) || false;
 		}
 	,"Cuit no v&aacute;lido");
 };
