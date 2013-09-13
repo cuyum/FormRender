ffac9c3f5b2491447a84df616b6498e4b7cdc5b6
gleotta <gleotta@cuyum.com>
2013-09-13 14:11:31 -0300

    cambio de envio json a traves de atributo value

--- src/main/java/ar/com/cuyum/cnc/service/rest/ExternalRest.java
+++ src/main/java/ar/com/cuyum/cnc/service/rest/ExternalRest.java
@@ -10,12 +10,18 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import org.jboss.logging.Param;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import ar.com.cuyum.cnc.service.JsonServices;
 import ar.com.cuyum.cnc.service.RelayService;
@@ -33,25 +39,44 @@ public class ExternalRest {
 	@Inject
 	private JsonServices jsonService;
 
-	@SuppressWarnings("finally")
 	@POST
 	@Path("/preview")
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("text/html")
 	public String previewHtml(@Context ServletContext servletContext,
-			String json) {
-
+			@FormParam("value") String json) {
+		
+		//formate el json antes de continuar
+		try {
+			JSONObject ojson = (JSONObject) ((new JSONParser()).parse(json));
+			json = ojson.toJSONString();
+		} catch (ParseException e2) {
+			e2.printStackTrace();
+			try {
+				response.sendError(400, "Error en conversion de json");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return e2.getMessage();
+		}
+		
+		System.out.println("value="+json);
 		String ret;
 		try {
 			ret = jsonService.previewHtml(json, servletContext);
 			return ret;
 		} catch (Exception e) {
+			e.printStackTrace();
 			// no se si es la mejor manera de mandar error
-			response.sendError(400, "Error en conversion de json");
-
-		} finally {
-			return "";
-		}
+			try {
+				response.sendError(400, "Error en conversion de json");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return e.getMessage();
+		} 
 	}
 
 	@POST
@@ -81,12 +106,13 @@ public class ExternalRest {
 	RelayService relay;
 
 	@POST
-	@GET
 	@Path("/preview2")
-	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("text/html; charset=utf-8")
-	public String formConsumerHTML() {
+	public String formConsumerHTML(@Context ServletContext servletContext,
+			@FormParam("value") String json) {
 		String builder = null;
+		System.out.println("value="+json);
 		// log.error(a.getHostAddress());
 		// log.error(sc.getContextPath());
 		// try {
@@ -119,8 +145,8 @@ public class ExternalRest {
 //			System.out.println("request.getServletContext().getServletContextName(): "+request.getServletContext().getServletContextName());
 //			System.out.println("request.getServletContext().getContextPath(): "+request.getServletContext().getContextPath());
 
-			
-			builder = relay.submit(url, null);
+			//relay.retrieve(remoteUrl)
+			builder = relay.retrieve(url);
 
 		} catch (Exception e) {
 			// Handle generic exceptions
