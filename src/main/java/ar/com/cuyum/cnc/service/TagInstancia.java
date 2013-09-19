2484443578b3094c532552ab5b22913a29596b88
Jorge Morando <jmorando@cuyum.com>
2013-09-19 11:51:28 -0300

    clase que permite crear los tags de la parte de Instancia del XML

---
+++ src/main/java/ar/com/cuyum/cnc/service/TagInstancia.java
@@ -0,0 +1,37 @@
+package ar.com.cuyum.cnc.service;
+
+public class TagInstancia {
+	private Boolean abrir;
+	private String tag;
+	private Boolean cerrar;
+
+	TagInstancia(Boolean abrir, String tag, Boolean cerrar) {
+		this.abrir = abrir;
+		this.tag = tag;
+		this.cerrar = cerrar;
+	}
+
+	public Boolean getAbrir() {
+		return abrir;
+	}
+
+	public Boolean getCerrar() {
+		return cerrar;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setAbrir(Boolean abrir) {
+		this.abrir = abrir;
+	}
+
+	public void setCerrar(Boolean cerrar) {
+		this.cerrar = cerrar;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+}
