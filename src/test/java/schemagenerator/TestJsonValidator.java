01fba003e026de8c1296c34c67d2394d33e2369d
ltroconis <ltroconis@cuyum.com>
2013-11-01 19:23:26 -0300

    carga masiva json data con la forma {     "formulario": {         "id": "C1.14",         "formularios": [             {                 "data": [                     {los valores del formulario}                 ]             },             {                 "data": [                     {los valores del formulario}                 ]             }         ]     } }

--- src/test/java/schemagenerator/TestJsonValidator.java
+++ src/test/java/schemagenerator/TestJsonValidator.java
@@ -4,6 +4,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
@@ -18,6 +19,8 @@ import com.github.fge.jsonschema.load.configuration.LoadingConfiguration;
 import com.github.fge.jsonschema.load.configuration.LoadingConfigurationBuilder;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.github.fge.jsonschema.report.LogLevel;
+import com.github.fge.jsonschema.report.ProcessingMessage;
 import com.github.fge.jsonschema.report.ProcessingReport;
 
 public class TestJsonValidator {
@@ -25,7 +28,11 @@ public class TestJsonValidator {
 
 	@BeforeClass
 	public static void startup() {
-		BasicConfigurator.configure();
+		if (log.isTraceEnabled()) {
+			log.info("log configurado");
+		} else {
+			BasicConfigurator.configure();
+		}
 	}
 
 	@Test
@@ -66,11 +73,17 @@ public class TestJsonValidator {
 
 		ProcessingReport report = schema.validate(data);
 
-		// log.debug(report);
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
 
 		assertThat(report.isSuccess()).isTrue();
 
-		log.info("C1.1 Ok ");
+		log.info("**** C1.1 está Ok en 1er test ****");
 	}
 
 	@Test
@@ -106,15 +119,21 @@ public class TestJsonValidator {
 
 		ProcessingReport report = schema.validate(data);
 
-		// log.info(report);
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
 
 		assertThat(report.isSuccess()).isTrue();
 
-		log.info("C1.1 Ok ");
+		log.info("**** C1.1 está Ok en 2do test ****");
 	}
 
 	@Test
-	public final void test_C1_3_OK() throws JsonProcessingException,
+	public final void test_C1_3_OK1() throws JsonProcessingException,
 			FileNotFoundException, IOException, ProcessingException {
 
 		String actualPath = "file:///"
@@ -146,15 +165,67 @@ public class TestJsonValidator {
 
 		ProcessingReport report = schema.validate(data);
 
-		// log.info(report);
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
 
 		assertThat(report.isSuccess()).isTrue();
 
-		log.info("C1.3 Ok ");
+		log.info("**** C1.3 está Ok en 1er test ****");
 	}
-
+	
 	@Test
-	public final void test_C1_10_OK() throws JsonProcessingException,
+	public final void test_C1_3_OK2() throws JsonProcessingException,
+			FileNotFoundException, IOException, ProcessingException {
+
+		String actualPath = "file:///"
+				+ this.getClass().getResource("..").getPath(), schemaPath = actualPath
+				+ "formularios/schemas/C1.3-schema.json", schemaGeneralPath = actualPath
+				+ "formularios/schemas/formulario.json";
+
+		final LoadingConfigurationBuilder builder = LoadingConfiguration
+				.newBuilder();
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		final JsonNode schemaNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/C1.3-schema.json"));
+
+		final JsonNode schemaFormNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/formulario.json"));
+
+		builder.preloadSchema(schemaPath, schemaNode);
+		builder.preloadSchema(schemaGeneralPath, schemaFormNode);
+
+		final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
+				.setLoadingConfiguration(builder.freeze()).freeze();
+
+		final JsonNode data = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/data/C1.3_OK2.json"));
+
+		final JsonSchema schema = factory.getJsonSchema(schemaPath);
+
+		ProcessingReport report = schema.validate(data);
+
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
+
+		assertThat(report.isSuccess()).isTrue();
+
+		log.info("**** C1.3 está Ok en 2do test ****");
+	}
+	
+	@Test
+	public final void test_C1_10_OK1() throws JsonProcessingException,
 			FileNotFoundException, IOException, ProcessingException {
 
 		String actualPath = "file:///"
@@ -186,17 +257,73 @@ public class TestJsonValidator {
 
 		ProcessingReport report = schema.validate(data);
 
-		// log.info(report);
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
 
 		assertThat(report.isSuccess()).isTrue();
-		
-		log.info("C1.10 Ok ");
-	}
 
+		log.info("**** C1.10 está Ok en 1er test ****");
+
+		assertThat(report.isSuccess()).isTrue();		
+	}
+	
 	@Test
-	public final void test_C1_14_OK() throws JsonProcessingException,
+	public final void test_C1_10_OK2() throws JsonProcessingException,
 			FileNotFoundException, IOException, ProcessingException {
 
+		String actualPath = "file:///"
+				+ this.getClass().getResource("..").getPath(), schemaPath = actualPath
+				+ "formularios/schemas/C1.10-schema.json", schemaGeneralPath = actualPath
+				+ "formularios/schemas/formulario.json";
+
+		final LoadingConfigurationBuilder builder = LoadingConfiguration
+				.newBuilder();
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		final JsonNode schemaNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/C1.10-schema.json"));
+
+		final JsonNode schemaFormNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/formulario.json"));
+
+		builder.preloadSchema(schemaPath, schemaNode);
+		builder.preloadSchema(schemaGeneralPath, schemaFormNode);
+
+		final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
+				.setLoadingConfiguration(builder.freeze()).freeze();
+
+		final JsonNode data = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/data/C1.10_OK2.json"));
+
+		final JsonSchema schema = factory.getJsonSchema(schemaPath);
+
+		ProcessingReport report = schema.validate(data);
+
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
+
+		assertThat(report.isSuccess()).isTrue();
+
+		log.info("**** C1.10 está Ok en 2do test ****");
+
+		assertThat(report.isSuccess()).isTrue();		
+	}
+	  
+	 
+	@Test
+	public final void test_C1_14_OK1() throws JsonProcessingException,
+			FileNotFoundException, IOException, ProcessingException {
 
 		String actualPath = "file:///"
 				+ this.getClass().getResource("..").getPath(), schemaPath = actualPath
@@ -227,11 +354,165 @@ public class TestJsonValidator {
 
 		ProcessingReport report = schema.validate(data);
 
-		// log.info(report);
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
+
+		assertThat(report.isSuccess()).isTrue();
+
+		log.info("**** C1.14 está Ok en 1er test ****");
+
+		assertThat(report.isSuccess()).isTrue();
+	}
+	
+	@Test
+	public final void test_C1_14_OK2() throws JsonProcessingException,
+			FileNotFoundException, IOException, ProcessingException {
+
+		String actualPath = "file:///"
+				+ this.getClass().getResource("..").getPath(), schemaPath = actualPath
+				+ "formularios/schemas/C1.14-schema.json", schemaGeneralPath = actualPath
+				+ "formularios/schemas/formulario.json";
+
+		final LoadingConfigurationBuilder builder = LoadingConfiguration
+				.newBuilder();
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		final JsonNode schemaNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/C1.14-schema.json"));
+
+		final JsonNode schemaFormNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/formulario.json"));
+
+		builder.preloadSchema(schemaPath, schemaNode);
+		builder.preloadSchema(schemaGeneralPath, schemaFormNode);
+
+		final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
+				.setLoadingConfiguration(builder.freeze()).freeze();
+
+		final JsonNode data = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/data/C1.14_OK2.json"));
+
+		final JsonSchema schema = factory.getJsonSchema(schemaPath);
+
+		ProcessingReport report = schema.validate(data);
+
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
+
+		assertThat(report.isSuccess()).isTrue();
+
+		log.info("**** C1.14 está Ok en 2do test ****");
+
+		assertThat(report.isSuccess()).isTrue();
+	}
+	  
+	@Test
+	public final void test_C1_15_OK() throws JsonProcessingException,
+			FileNotFoundException, IOException, ProcessingException {
+
+		String actualPath = "file:///"
+				+ this.getClass().getResource("..").getPath(), schemaPath = actualPath
+				+ "formularios/schemas/C1.15-schema.json", schemaGeneralPath = actualPath
+				+ "formularios/schemas/formulario.json";
+
+		final LoadingConfigurationBuilder builder = LoadingConfiguration
+				.newBuilder();
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		final JsonNode schemaNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/C1.15-schema.json"));
+
+		final JsonNode schemaFormNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/formulario.json"));
+
+		builder.preloadSchema(schemaPath, schemaNode);
+		builder.preloadSchema(schemaGeneralPath, schemaFormNode);
+
+		final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
+				.setLoadingConfiguration(builder.freeze()).freeze();
+
+		final JsonNode data = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/data/C1.15_OK1.json"));
+
+		final JsonSchema schema = factory.getJsonSchema(schemaPath);
+
+		ProcessingReport report = schema.validate(data);
+
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
+
+		assertThat(report.isSuccess()).isTrue();
+
+		log.info("**** C1.15 está Ok en 1er test ****");
 
 		assertThat(report.isSuccess()).isTrue();
 		
-		log.info("C1.14 Ok ");
 	}
+	
+	@Test
+	public final void test_C1_15_OK2() throws JsonProcessingException,
+			FileNotFoundException, IOException, ProcessingException {
 
+		String actualPath = "file:///"
+				+ this.getClass().getResource("..").getPath(), schemaPath = actualPath
+				+ "formularios/schemas/C1.15-schema.json", schemaGeneralPath = actualPath
+				+ "formularios/schemas/formulario.json";
+
+		final LoadingConfigurationBuilder builder = LoadingConfiguration
+				.newBuilder();
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		final JsonNode schemaNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/C1.15-schema.json"));
+
+		final JsonNode schemaFormNode = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/schemas/formulario.json"));
+
+		builder.preloadSchema(schemaPath, schemaNode);
+		builder.preloadSchema(schemaGeneralPath, schemaFormNode);
+
+		final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
+				.setLoadingConfiguration(builder.freeze()).freeze();
+
+		final JsonNode data = mapper.readTree(this.getClass()
+				.getResourceAsStream("/formularios/data/C1.15_OK2.json"));
+
+		final JsonSchema schema = factory.getJsonSchema(schemaPath);
+
+		ProcessingReport report = schema.validate(data);
+
+		Iterator<ProcessingMessage> messages = report.iterator();
+		while (messages.hasNext()) {
+			ProcessingMessage msj = messages.next();
+			if (LogLevel.ERROR.equals(msj.getLogLevel())) {				
+				log.debug(msj.getMessage()+" >>"+msj.asJson().get("instance").get("pointer").asText());
+			}
+		}
+
+		assertThat(report.isSuccess()).isTrue();
+
+		log.info("**** C1.15 está Ok en 2do test ****");
+
+		assertThat(report.isSuccess()).isTrue();
+		
+	}
+	 
 }
