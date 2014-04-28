99f1670570e3030f5425890b85517fa22c1c1831
sabrina <sintroini@cuyum.com>
2014-04-28 11:07:26 -0300

    Actualización formularios D1.26 y E1.23 #326

--- sql/datos-DELTA_incremental-20131126.sql
+++ sql/datos-DELTA_incremental-20131126.sql
@@ -347,19 +347,10 @@ UPDATE formulario SET parametrosurl = 'id=ict4.2.5-A&repeat=1&periodicidad=anual
 
 UPDATE formulario SET parametrosurl = 'id=ict4.2.1-A&repeat=1&periodicidad=trimestral' WHERE (codigo = 'ict4.2.1-A');
 
---ANEXOS--
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version,xsltransform_id) VALUES ('anexoI','anexoI','anexoI-V001-FEB14','anexoI.xml', 0,1);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version,xsltransform_id) VALUES ('anexoII','anexoII','anexoII-V001-FEB14','anexoII.xml', 0,1);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version,xsltransform_id) VALUES ('anexoIII','anexoIII','anexoIII-V001-FEB14','anexoIII.xml', 0,1);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version,xsltransform_id) VALUES ('anexoIV','anexoIV','anexoIV-V001-FEB14','anexoIV.xml', 0,1);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version,xsltransform_id) VALUES ('anexoV','anexoV','anexoV-V001-FEB14','anexoV.xml', 0,1);
-INSERT INTO formulario (codigo, nombre, formVersion, archivo, version,xsltransform_id) VALUES ('anexoVI','anexoVI','anexoVI-V001-FEB14','anexoVI.xml', 0,1);
+--Update 28-04-2014
 
-UPDATE formulario SET parametrosurl = 'id=anexoI' WHERE (codigo = 'anexoI');
-UPDATE formulario SET parametrosurl = 'id=anexoII' WHERE (codigo = 'anexoII');
-UPDATE formulario SET parametrosurl = 'id=anexoIII' WHERE (codigo = 'anexoIII');
-UPDATE formulario SET parametrosurl = 'id=anexoIV' WHERE (codigo = 'anexoIV');
-UPDATE formulario SET parametrosurl = 'id=anexoV' WHERE (codigo = 'anexoV');
-UPDATE formulario SET parametrosurl = 'id=anexoVI' WHERE (codigo = 'anexoVI');
+UPDATE formulario SET parametrosurl = 'id=E1.23&repeat=1' WHERE (codigo = 'E1.23');
+UPDATE formulario SET parametrosurl = 'id=D1.26&repeat=1' WHERE (codigo = 'D1.26');
+
 
 
