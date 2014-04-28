3f799723b1ed6e4cdf623c9d9fde0802b94db6ea
sabrina <sintroini@cuyum.com>
2014-04-28 14:57:13 -0300

    Actualizacion de formularios #326

---
+++ sql/20140428/datos.sql
@@ -0,0 +1,4 @@
+--Update 28-04-2014
+
+UPDATE formulario SET parametrosurl = 'id=E1.23&repeat=1' WHERE (codigo = 'E1.23');
+UPDATE formulario SET parametrosurl = 'id=D1.26&repeat=1' WHERE (codigo = 'D1.26');
